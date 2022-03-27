258d2a6dfde1c6f97f6656ecadd1d32d078e38ad
Mathieu Schroeter <mathieu@schroetersa.ch>
2022-03-27 23:09:20 +0200

    Fix stupid mistake

--- wpkg/libdebpackages/wpkgar_build.cpp
+++ wpkg/libdebpackages/wpkgar_build.cpp
@@ -2687,13 +2687,13 @@ int wpkgar_build::update_and_upgrade()
         cmd += " --instdir ";
         cmd += f_manager->get_inst_path().full_path();
         cmd += " --admindir ";
+        cmd += f_manager->get_database_path().full_path();
         const std::string& tmpdir(wpkg_filename::temporary_uri_filename::get_tmpdir());
         if(!tmpdir.empty())
         {
             cmd += " --tmpdir ";
             cmd += wpkg_util::make_safe_console_string(tmpdir);
         }
-        cmd += f_manager->get_database_path().full_path();
         cmd += " --" + step + " ";
 
         if(step == "upgrade")
